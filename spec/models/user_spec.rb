708255404173fa3458edbc89b7fea2223c66b182
ohirune41 <jiben8628@gmail.com>
2022-05-27 14:26:04 +0900

    単体テストに関する修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -82,7 +82,13 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
-      it 'passwordが半角英数字混合でないと登録できない' do
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it '数字のみのパスワードでは登録できない' do
         @user.password = '111111'
         @user.password_confirmation = '111111'
         @user.valid?
