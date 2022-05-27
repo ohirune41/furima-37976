d83aee2f17718ac364a4e9181d5c700aec6f36d4
ohirune41 <jiben8628@gmail.com>
2022-05-27 15:55:16 +0900

    Active Storageの導入、アクションとルーティングの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  vaildates :quality_id, presence: true
+  vaildates :payment_id, presence: true
+  vaildates :area_id, presence: true
+  vaildates :preparation_id, presence: true
+  vaildates :price, presence: true
 end
