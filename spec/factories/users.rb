cdc68f48897d731548f57573de3e657c5d44bd94
ohirune41 <jiben8628@gmail.com>
2022-05-27 11:54:45 +0900

    モデルの単体テストの実施

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 15)}
+    password_confirmation {password}
+    gimei = Gimei.name
+    last_name             {gimei.last}
+    first_name            {gimei.first}
+    last_name_katakana    {gimei.last.katakana}
+    first_name_katakana   {gimei.first.katakana}
+    birth_day             { Faker::Date.backward }
+  end
+end
