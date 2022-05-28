5f565cbae92816f833f658d344bc581cd0833e0c
ohirune41 <jiben8628@gmail.com>
2022-05-28 11:02:07 +0900

    ActiveHashを導入して、商品出品ページを表示出来るようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
   end
   def new
+    @item = Item.new
   end
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :quality_id, :payment_id, :area_id, :preparation_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :payment
+  belongs_to :area
+  belongs_to :preparation
+
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  vaildates :quality_id, presence: true
-  vaildates :payment_id, presence: true
-  vaildates :area_id, presence: true
-  vaildates :preparation_id, presence: true
-  vaildates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :preparation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 end
