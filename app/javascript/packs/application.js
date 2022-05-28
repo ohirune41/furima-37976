45ffea0e349d83f6f6403234e2d2e9d6938a48ff
ohirune41 <jiben8628@gmail.com>
2022-05-28 19:11:46 +0900

    販売手数料や販売利益の計算結果が表示されるようにする

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../commission");
+require("../profit");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
