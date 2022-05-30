201786a544651fe9224eba612bced33befaff3ac
ohirune41 <jiben8628@gmail.com>
2022-05-30 19:55:00 +0900

    モデルの単体テストの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,15 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :preparation
 
-  validates :item_name, presence: true
-  validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :payment_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :preparation_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  with_options presence: true do
+    validates :image
+    validates :item_name, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }, inclusion: { in: 300..9999999, message: 'is out of setting range' }
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :payment_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :preparation_id, numericality: { other_than: 1 , message: "can't be blank"}
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name         {Faker::Team.name}
+    explanation       {Faker::Lorem.sentence}
+    category_id       {Faker::Number.between(from: 2, to: 11)}
+    quality_id        {Faker::Number.between(from: 2, to: 7)}
+    payment_id        {Faker::Number.between(from: 2, to: 3)}
+    area_id           {Faker::Number.between(from: 2, to: 48)}
+    preparation_id    {Faker::Number.between(from: 2, to: 4)}
+    price             {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
