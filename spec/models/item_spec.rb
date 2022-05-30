a0f3a0e6df29788e1c1f012f8261f378580f647b
ohirune41 <jiben8628@gmail.com>
2022-05-30 21:11:30 +0900

    コードレビューで指摘された所の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -82,6 +82,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Preparation can't be blank")
       end
+      it 'userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
