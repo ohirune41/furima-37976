ac37a6d3566e73c9510647dbafdfacd4d7423b89
ohirune41 <jiben8628@gmail.com>
2022-06-02 14:58:35 +0900

    レビュー依頼で指摘された所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -35,9 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
+    if user_signed_in?
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
