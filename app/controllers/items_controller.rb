2901f5adb79402f2991d5a5e5f9782b5d2e484d8
ohirune41 <jiben8628@gmail.com>
2022-06-02 15:08:06 +0900

    レビュー依頼で指摘された所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in?
+    if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
     end
